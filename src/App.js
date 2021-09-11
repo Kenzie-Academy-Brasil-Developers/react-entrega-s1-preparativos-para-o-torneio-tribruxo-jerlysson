1475286d7be478c1b5f83c6a3c35266650cdc034
Jerlysson Miranda <jerlyssonm@gmail.com>
2021-09-10 22:50:44 -0300

    erro fetch

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     fetch("http://hp-api.herokuapp.com/api/characters/students")
       .then((response) => response.json())
       .then((response) => setStudents(response))
-      .catch((err) => alert(`${err}`));
+      .catch((err) => `${err}`);
   }, []);
 
   // ============ SEPARAÇÃO DOS TIMES POR CASAS=============
